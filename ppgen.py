6cc40fabaa5324e2342ca9031ea8dcfbb04f91f4
wf49670 <walt.farrell@charter.net>
2014-11-30 10:42:19 -0500

    3.43d Fix erroneous sc warning within .nf block
    Normally ppgen will warn the PPer if it sees an all lower-case string of
    text within sc /sc tags, as we never want all lower-case small-cap
    strings. That case should be prepared by the formatters and the PPer
    as all upper-case within the tags, instead.
    
    However, for text within .nf blocks ppgen outputs each line in a div, and
    so it needs to replicate any inline tags that span lines. This can result
    in a line that is both all lower-case and within the sc tags, although the
    complete string as prepared by the formatters and PPer was in fact
    mixed-case. So, within a .nf block, ppgen will suppress that warning
    message.

--- ppgen.py
+++ ppgen.py
@@ -15,7 +15,7 @@ import struct
 import imghdr
 import traceback
 
-VERSION="3.43c"  # 27-Nov-2014
+VERSION="3.43d"  # 27-Nov-2014
 
 NOW = strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " GMT"
 
@@ -2865,11 +2865,16 @@ class Pph(Book):
       self.wb[i] = re.sub(r"<B", "<b", self.wb[i])
       self.wb[i] = re.sub(r"<\/B", "</b", self.wb[i])
 
+    inNF = False
     for i, line in enumerate(self.wb):
 
       # if everything inside <sc>...</sc> markup is uppercase, then
       # use font-size:smaller, else use font-variant:small-caps
 
+      if self.wb[i].startswith(".nf "):
+        inNF = True
+      elif self.wb[i].startswith(".nf-"):
+        inNF = False
       m = re.search("<sc>", self.wb[i]) # opening small cap tag
       if m:
         use_class = "sc" # unless changed
@@ -2883,7 +2888,11 @@ class Pph(Book):
         m = re.search(r"<sc>([^<]+?)</sc>", stmp)
         if m:
           scstring = m.group(1)
-          if scstring == scstring.lower(): # all lower case
+          # warn about all lower case, but not within .nf as
+          # we will have replicated the <sc> tags that cross lines
+          # of the .nf block, which could leave some all lower-case
+          # line alone within the <sc> </sc>, but it's not an error
+          if not inNF and scstring == scstring.lower():
             self.warn("all lower case inside small-caps markup: {}".format(self.wb[i]))
           if scstring == scstring.upper(): # all upper case
             use_class = "fss"
