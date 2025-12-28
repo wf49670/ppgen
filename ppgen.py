83e14c16f4b306422c14683b6412f37c959d5fcb
wf49670 <wf49670@users.noreply.github.com>
2025-12-28 16:28:22 -0500

    Migrate changes for PR123 from develop to Test (3.57i)
    Make 3.57i available to the wider community on the Test branch.

--- ppgen.py
+++ ppgen.py
@@ -35,7 +35,7 @@ import struct
 import traceback
 import json
 
-VERSION="3.57h" + with_regex   # 26-Dec-2025
+VERSION="3.57i" + with_regex   # 28-Dec-2025
 #3.57a:
 #  Initial 3.57 release
 #  Enh: Provide context for "Unclosed tags in .nf block" error
@@ -75,6 +75,8 @@ VERSION="3.57h" + with_regex   # 26-Dec-2025
 #3.57h:
 #  Bug: Line rewrap issue in centered text
 #  Bug: Pending vertical space before an illustration emitted after illo in HTML
+#3.57i:
+#  Bug: Issue 117, PR 123: Output files on Windows ended up double-spaced after the 3.57g changes to codecs.open(). 
 
 NOW = strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " GMT"
 
@@ -1909,7 +1911,7 @@ class Book(object):
 
   def cvglist(self):
     if self.listcvg:
-      f1 = open("ppgen-cvglist.txt", "w", encoding="UTF-8")
+      f1 = open("ppgen-cvglist.txt", "w", encoding="UTF-8", newline='')
       f1.write("\r\n\r\nppgen {}\r\n".format(VERSION))
       f1.write("\r\nBuilt-in Greek Characters:\r\n")
       f1.write("User enters:  ppgen generates:\r\n\r\n")
@@ -1932,7 +1934,7 @@ class Book(object):
   # Create special output file after .gk or .cv if requested, and quit
   def cvgbailout(self):
     bailfn = re.sub("-src", "", self.srcfile.split('.')[0]) + "-cvgout-utf8.txt"
-    f1 = open(bailfn, "w", encoding="UTF-8")
+    f1 = open(bailfn, "w", encoding="UTF-8", newline='')
     for index,t in enumerate(self.wb):
       # unprotect temporarily protected characters from Greek strings
       t = t.replace("⑩", r"\|") # restore temporarily protected \| and \(space)
@@ -1967,7 +1969,7 @@ class Book(object):
       self.ppqt.append('  ]')
       self.ppqt.append('}')
       ppqtfn = fn + ".ppqt"
-      f1 = open(ppqtfn, "w", encoding="ISO-8859-1")
+      f1 = open(ppqtfn, "w", encoding="ISO-8859-1", newline='')
       for index,t in enumerate(self.ppqt):
         f1.write("{:s}\r\n".format(t))
       f1.close()
@@ -2008,7 +2010,7 @@ class Book(object):
       bb.append(r"$::pngspath = '{}';".format(os.path.join(os.path.dirname(os.path.realpath(self.srcfile)),"pngs","")))
       bb.append("1;")
       binfn = self.srcfile + ".bin"
-      f1 = open(binfn, "w", encoding="ISO-8859-1")
+      f1 = open(binfn, "w", encoding="ISO-8859-1", newline='')
       for index,t in enumerate(bb):
         f1.write("{:s}\r\n".format(t))
       f1.close()
@@ -5072,7 +5074,7 @@ class Ppt(Book):
       encoding = "UTF-8"
     else:
       encoding = "ISO-8859-1"
-    f1 = open("bailout.txt", "w", encoding=encoding)
+    f1 = open("bailout.txt", "w", encoding=encoding, newline='')
     for index,t in enumerate(buffer):
       f1.write( "{:s}\r\n".format(t.rstrip()) )
     f1.close()
@@ -6070,7 +6072,7 @@ class Ppt(Book):
     longcount = 0
     while (len(self.eb) > 0) and not self.eb[-1]:
       self.eb.pop()
-    f1 = open(fn, "w", encoding="utf-8")
+    f1 = open(fn, "w", encoding="utf-8", newline='')
     for index,t in enumerate(self.eb):
       s = t.rstrip()
       if self.truelen(s) > self.linelimitwarning:
@@ -6086,7 +6088,7 @@ class Ppt(Book):
     if self.bnPresent:
       fnb = fn + ".bin"
       fnj = fn + ".json"
-      f1 = open(fnb, "w", encoding="ISO-8859-1")
+      f1 = open(fnb, "w", encoding="ISO-8859-1", newline='')
       for index,t in enumerate(self.bb):
         f1.write("{:s}\r\n".format(t))
       f1.close()
@@ -6144,7 +6146,7 @@ class Ppt(Book):
     # write Latin-1 file (text output, Latin-1)
     # note: using open allows specific line terminators.
     # using .open would write platform-specific line terminators.
-    f1 = open(fn, "w", encoding="ISO-8859-1")
+    f1 = open(fn, "w", encoding="ISO-8859-1", newline='')
     longcount = 0
     for index,t in enumerate(self.eb):
       s = t.rstrip()
@@ -6160,7 +6162,7 @@ class Ppt(Book):
     # save GG .bin file if needed
     if self.bnPresent:
       fnb = fn + ".bin"
-      f1 = open(fnb, "w", encoding="ISO-8859-1")
+      f1 = open(fnb, "w", encoding="ISO-8859-1", newline='')
       for index,t in enumerate(self.bb):
         f1.write("{:s}\r\n".format(t))
       f1.close()
@@ -8196,7 +8198,7 @@ class Pph(Book):
 
   # bailout after saving working buffer in bailout.txt
   def bailout(self, buffer):
-    f1 = open("bailout.txt", "w", encoding='utf-8')
+    f1 = open("bailout.txt", "w", encoding='utf-8', newline='')
     for index,t in enumerate(buffer):
       f1.write( "{:s}\r\n".format(t.rstrip()) )
     f1.close()
@@ -9049,7 +9051,7 @@ class Pph(Book):
   # -------------------------------------------------------------------------------------
   # save buffer to specified dstfile (HTML output)
   def saveFile(self, fn):
-    f1 = open(fn, "w", encoding=self.encoding)
+    f1 = open(fn, "w", encoding=self.encoding, newline='')
     for index,t in enumerate(self.wb):
       try:
         f1.write( "{:s}\r\n".format(t))
@@ -9062,7 +9064,7 @@ class Pph(Book):
     if self.bnPresent:
       fnb = fn + ".bin"
       fnj = fn + ".json"
-      f1 = open(fnb, "w", encoding="ISO-8859-1")
+      f1 = open(fnb, "w", encoding="ISO-8859-1", newline='')
       for index,t in enumerate(self.bb):
         f1.write("{:s}\r\n".format(t))
       f1.close()
